3589a916c8f07969c2afb9ce4dbce91f96e5572d
HIDEKAZU SHINOHARA <141484383+zetjrgfhck@users.noreply.github.com>
2024-05-29 16:44:19 +0000

    5/30

---
+++ chapter3.java
@@ -0,0 +1,7 @@
+public class chapter3 {
+  public static void main(String[] args) {
+    //System.out.println(17); 17を出力
+    //System.out.println("10 + 7"); 10 + 7を出力
+    System.out.println(10 + 7); //10 + 7を計算した結果を出力
+  }
+}
