51968c5885910be1539ba75977263be58cbb5575
HIDEKAZU SHINOHARA <141484383+zetjrgfhck@users.noreply.github.com>
2024-05-29 16:29:07 +0000

    5/30

---
+++ chapter1.java
@@ -0,0 +1,5 @@
+public class chapter1{
+  public static void main(String[] args){
+    System.out.println("ここに入力");
+  }
+}
---
+++ chapter2.java
@@ -0,0 +1,6 @@
+public class chapter2{
+  public static void main(String[] args){
+    //この部分にコメント入力
+    System.out.println("ここに入力");
+  }
+}
