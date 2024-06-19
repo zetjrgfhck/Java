43b364c8e940b650bdb814fc26771d18b4209776
HIDEKAZU SHINOHARA <141484383+zetjrgfhck@users.noreply.github.com>
2024-06-19 05:51:53 +0000

    6/19

--- chapter4.java
+++ chapter4.java
@@ -1,7 +1,7 @@
 public class chapter4{
   public static void main(String[] args){
-    //System.out.println(12 / 3);
-    //System.out.println(3 * 6);
-    System.out.println(8 % 3);
+    //System.out.println(12 / 3);  割り算
+    //System.out.println(3 * 6);  掛け算
+    System.out.println(8 % 3);  //余り
   }
 }
---
+++ chapter8.java
@@ -0,0 +1,13 @@
+public class chapter8{
+  public static void main(String[] args){
+    int number = 11;
+    String text = "Ruby";
+    System.out.println(number);
+    System.out.println(text);
+
+    number = 9;  //定義を上書き データ型を書かない
+    System.out.println(number);
+    text = "Java";  //定義を上書き データ型を書かない
+    System.out.println(text);
+  }
+}
