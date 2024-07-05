41047cc9a30c11eb49fd2950c6663c3384ce31f3
HIDEKAZU SHINOHARA <141484383+zetjrgfhck@users.noreply.github.com>
2024-07-05 12:47:25 +0000

    7/5

--- chapter22.java
+++ chapter22.java
@@ -1,9 +1,11 @@
 public class chapter22 {
   public static void main(String[] args) {
-    String[] names = {"にんじゃわんこ" , "ひつじ仙人" , "ベイビーわんこ"}; //{}で配列を記述
+    String[] names = {"にんじゃわんこ" , "ひつじ仙人" , "ベイビーわんこ"}; //{}で配列を記述、[]の前に間空けない
     System.out.println(names[0]);
-    System.out.println(names[2]);
+    System.out.println(names[1]);
     //インデックス番号が0の要素を出力してください。
     //インデックス番号が2の要素を出力してください。
+    names[1] = "Java"; //上書き
+    System.out.println(names[1]);
   }
 }
---
+++ chapter23.java
@@ -0,0 +1,15 @@
+public class chapter23 {
+  public static void main(String[] args) {
+    String[] names ={"にんじゃわんこ" , "ひつじ仙人" , "ベイビーわんこ"};
+
+    // for文を用いて、「私の名前は◯◯です」と出力してください
+    for(int i = 0; i < names.length; i++){
+      System.out.println("私の名前は" + names[i] + "です");
+    }
+    
+    //拡張for文
+    for(String name: names) {
+      System.out.println("私の名前は" + name + "です");
+    }
+  }
+}
